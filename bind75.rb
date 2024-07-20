2e9a00890a19eb10ac3cbd74fb0e8d7cb818803b
Jaspreet Singh <99jasingh@gmail.com>
2024-07-19 22:16:13 -0400

    place_flower

--- bind75.rb
+++ bind75.rb
@@ -17,3 +17,23 @@ def merge_alternately(word1, word2)
     end
     return res
 end
+
+def can_place_flowers(flowerbed, n)
+    return true if n==0
+    i = 0 
+
+    while (i<flowerbed.length)
+        if flowerbed[i]==1
+            i+=2
+        elsif flowerbed[i]==0
+            if flowerbed[i+1]==0 || i== flowerbed.length-1
+                n-=1
+                i+=2
+            elsif flowerbed[i+1] == 1
+                i+=3
+            end
+        end
+        return true if n==0
+    end
+    return false
+end
