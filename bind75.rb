e319e22b23c268de4576e3cb2c22941a97e62e2f
Jaspreet Singh <99jasingh@gmail.com>
2024-08-24 21:36:42 -0400

    number of island using tree, looked up solution as I was stuck

--- bind75.rb
+++ bind75.rb
@@ -398,3 +398,37 @@ def find_min(nums)
     end
     min
 end
+
+#number of island using tree, used the solution as I was stuck in the problem will try to solve same problem next week
+def dfs(grid, i, j)
+    n = grid.length
+    m = grid[0].length
+    
+    if i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != '1'
+        return
+    end
+    
+    grid[i][j] = '2' # Mark as visited
+    
+    # Explore neighbors
+    dfs(grid, i+1, j) # Check bottom
+    dfs(grid, i-1, j) # Check top
+    dfs(grid, i, j+1) # Check right
+    dfs(grid, i, j-1) # Check left
+end
+
+def num_islands(grid)
+    count = 0
+    n = grid.length
+    m = grid[0].length
+    
+    for i in 0...n
+        for j in 0...m
+            if grid[i][j] == '1'
+                count += 1
+                dfs(grid, i, j)
+            end
+        end
+    end
+    return count
+end
