16d90afb4d682d3b1ca08459a796e6626948743f
Jaspreet Singh <99jasingh@gmail.com>
2024-09-21 21:59:13 -0400

    Palindrome number in java

--- bind75.rb
+++ bind75.rb
@@ -767,3 +767,15 @@ public boolean exist(char[][] board, String word) {
         visited[i][j] = false;
         return false;
     }
+
+#Palindrome Number
+public boolean isPalindrome(int x) {
+        String s=Integer.toString(x);
+        int n=s.length();
+        for(int i=0;i<n/2;i++){
+            if(s.charAt(i)!=s.charAt(n-i-1)){
+                return false;
+            }
+        }
+        return true;
+}
