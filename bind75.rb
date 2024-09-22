084b134d38d239296f61f0f4615ae2f08e87834a
Jaspreet Singh <99jasingh@gmail.com>
2024-09-21 22:03:11 -0400

    Find the Index of the First Occurrence in a String

--- bind75.rb
+++ bind75.rb
@@ -808,3 +808,18 @@ public int removeDuplicates(int[] arr) {
         return i+1;
         
 }
+
+#Find the Index of the First Occurrence in a String
+public int strStr(String haystack, String needle) {
+        if (haystack.length() < needle.length()) {
+            return -1;
+        }
+        
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            if (haystack.substring(i, i + needle.length()).equals(needle)) {
+                return i;
+            }
+        }
+        
+        return -1;        
+}
