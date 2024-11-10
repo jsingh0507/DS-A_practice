340ca0a8dbb3ddc39b2cb85ec428b917b59f3cca
Jaspreet Singh <99jasingh@gmail.com>
2024-11-09 19:47:32 -0500

    palindrome numbers

--- bin75Java.java
+++ bin75Java.java
@@ -65,4 +65,17 @@ class Solution {
         
         return isNegative ? -res : res;       
     }
+
+    //palindrome numbers
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
 }
