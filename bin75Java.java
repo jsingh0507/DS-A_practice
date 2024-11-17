27d8e50212b21a9781153e1ce8752a4334b75760
Jaspreet Singh <99jasingh@gmail.com>
2024-11-16 22:43:02 -0500

    finding first occurunce in string

--- bin75Java.java
+++ bin75Java.java
@@ -95,4 +95,18 @@ class Solution {
         }
         return str;
     }
+
+
+    //find index first occurrence in the string
+
+    public int strStr(String haystack, String needle) {
+        int m = haystack.length(), n = needle.length();
+        for (int i = 0; i <= m - n; i++) {
+            if (haystack.substring(i, i + n).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
+
