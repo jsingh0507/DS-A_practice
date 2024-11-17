316a87321c40627fbe59850c626061cf4fe2255b
Jaspreet Singh <99jasingh@gmail.com>
2024-11-16 22:45:02 -0500

    search insert position

--- bin75Java.java
+++ bin75Java.java
@@ -108,5 +108,30 @@ class Solution {
         }
         return -1;
     }
+
+    //Search insert position with binary search algortihm
+
+    public int searchInsert(int[] nums, int target) {
+        return binarySearch(nums, target);
+    }
+    
+    private int binarySearch(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            
+            if (nums[mid] > target) {
+                high = mid - 1;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        
+        return low;
+    }
 }
 
