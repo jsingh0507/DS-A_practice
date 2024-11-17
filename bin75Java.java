5c8f45791eb2b8673b00e358466da803f172d3b7
Jaspreet Singh <99jasingh@gmail.com>
2024-11-16 22:51:48 -0500

    adding binary numbers

--- bin75Java.java
+++ bin75Java.java
@@ -157,5 +157,21 @@ class Solution {
         result[0] = 1;
         return result;
     }
+
+    //add binary
+
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] newDigits = new int[digits.length + 1];
+        newDigits[0] = 1;
+        return newDigits;
+    }
 }
 
