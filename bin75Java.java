dc5206abda5efda094f520d908cbc2c81a017902
Jaspreet Singh <99jasingh@gmail.com>
2024-11-23 01:13:33 -0500

    3Some in java

--- bin75Java.java
+++ bin75Java.java
@@ -173,5 +173,39 @@ class Solution {
         newDigits[0] = 1;
         return newDigits;
     }
+
+    //3 Some in java
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+                int total = nums[i] + nums[j] + nums[k];
+
+                if (total > 0) {
+                    k--;
+                } else if (total < 0) {
+                    j++;
+                } else {
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+
+                    while (nums[j] == nums[j-1] && j < k) {
+                        j++;
+                    }
+                }
+            }
+        }
+        return res;        
+    }
 }
 
