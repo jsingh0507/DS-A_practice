70f1da5d33f5c45f7ba917154db1dedf20c3bcbd
Jaspreet Singh <99jasingh@gmail.com>
2024-11-30 23:35:50 -0500

    rotating image

--- bin75Java.java
+++ bin75Java.java
@@ -419,5 +419,26 @@ class Solution {
     return sb.length() == 0 ? "0" : sb.toString();
 }
 
+    //rotating image
+
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        // Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+    }
 }
 
