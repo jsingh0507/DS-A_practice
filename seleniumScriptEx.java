c26a17e551b9081b8665f3b283174ae36dfdee75
Jaspreet Singh <99jasingh@gmail.com>
2025-02-15 22:55:53 -0500

    Get Element Find By

--- seleniumScriptEx.java
+++ seleniumScriptEx.java
@@ -20,3 +20,19 @@ public class VerifyPageTitle {
         driver.quit();
     }
 }
+
+public class FindElementByIdAndClick {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://example.com"); // Replace with your target URL
+        
+        // Assume there's a button with the ID "myButton"
+        WebElement button = driver.findElement(By.id("myButton"));
+        button.click();
+        
+        Thread.sleep(2000); // Pause for 2 seconds to observe the click action
+        System.out.println("Clicked the element with ID 'myButton'.");
+        driver.quit();
+    }
+}
