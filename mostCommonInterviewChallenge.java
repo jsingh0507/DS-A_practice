66fc11a71eb767adab1d4a42571830c75573fa62
Jaspreet Singh <99jasingh@gmail.com>
2025-02-28 22:36:18 -0500

    Check Anagram

--- mostCommonInterviewChallenge.java
+++ mostCommonInterviewChallenge.java
@@ -101,3 +101,21 @@ public class RemoveDuplicates {
         System.out.println(Arrays.toString(removeDuplicates(numbers))); // [1, 2, 3, 4, 5]
     }
 }
+
+//Check anagram
+import java.util.Arrays;
+
+public class AnagramCheck {
+    public static boolean isAnagram(String str1, String str2) {
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        return Arrays.equals(arr1, arr2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("listen", "silent")); // true
+    }
+}
+
