eb8b9a3ebfa72b45cdad3b28abcd7f4ba510a913
Jaspreet Singh <99jasingh@gmail.com>
2025-02-28 22:31:26 -0500

    Factorial Recursion

--- mostCommonInterviewChallenge.java
+++ mostCommonInterviewChallenge.java
@@ -9,3 +9,14 @@ public class PalindromeCheck {
         System.out.println(isPalindrome("hello")); // false
     }
 }
+
+// factorial recursion
+public class Factorial {
+    public static int factorial(int n) {
+        return (n == 0) ? 1 : n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(5)); // 120
+    }
+}
