bdb728c3b1502e14955bee2f5345dc498b058409
Jaspreet Singh <99jasingh@gmail.com>
2025-03-08 22:42:09 -0500

    Binary search

--- mostCommonInterviewChallenge.java
+++ mostCommonInterviewChallenge.java
@@ -183,3 +183,29 @@ public class FirstNonRepeatingChar {
         System.out.println("First non-repeating character: " + firstUniqueChar("swiss")); // 'w'
     }
 }
+
+// BInary search implementation
+public class BinarySearch {
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid; // Target found
+            } else if (arr[mid] < target) {
+                left = mid + 1; // Search right half
+            } else {
+                right = mid - 1; // Search left half
+            }
+        }
+        return -1; // Target not found
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 3, 5, 7, 9, 11};
+        System.out.println("Index of 7: " + binarySearch(numbers, 7)); // 3
+        System.out.println("Index of 4: " + binarySearch(numbers, 4)); // -1
+    }
+}
