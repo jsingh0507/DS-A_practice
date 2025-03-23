12e5e1a1db373db28cc309bcb72a8e2f878dd64e
Jaspreet Singh <99jasingh@gmail.com>
2025-03-22 21:18:14 -0400

    Perfect NUmber

--- mostCommonInterviewChallenge.java
+++ mostCommonInterviewChallenge.java
@@ -494,3 +494,18 @@ public class MaxCharFrequency {
         System.out.println("Max occurring char: " + getMaxChar("aabccccddbbb")); // b
     }
 }
+
+public class PerfectNumber {
+    public static boolean isPerfect(int num) {
+        int sum = 0;
+        for (int i = 1; i <= num / 2; i++) {
+            if (num % i == 0) sum += i;
+        }
+        return sum == num;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPerfect(28)); // true
+        System.out.println(isPerfect(12)); // false
+    }
+}
