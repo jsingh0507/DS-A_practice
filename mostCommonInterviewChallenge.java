6a8e639eaa2368c75f748f9d0a98ca049cc7096e
Jaspreet Singh <99jasingh@gmail.com>
2025-03-22 21:16:46 -0400

    Fibionacci series

--- mostCommonInterviewChallenge.java
+++ mostCommonInterviewChallenge.java
@@ -431,3 +431,20 @@ public class CountVowels {
         System.out.println("Vowel Count: " + countVowels(input)); // Output: 5
     }
 }
+
+public class FibonacciSeries {
+    public static void printFibonacci(int n) {
+        int a = 0, b = 1;
+        System.out.print("Fibonacci Series: " + a + " " + b + " ");
+        for (int i = 2; i < n; i++) {
+            int c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+        }
+    }
+
+    public static void main(String[] args) {
+        printFibonacci(10); // 0 1 1 2 3 5 8 13 21 34
+    }
+}
