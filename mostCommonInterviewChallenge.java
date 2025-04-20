39131eea274add40661e876c1f10e5e3b79c011e
Jaspreet Singh <99jasingh@gmail.com>
2025-04-19 22:19:24 -0400

    Vowel count

--- mostCommonInterviewChallenge.java
+++ mostCommonInterviewChallenge.java
@@ -926,3 +926,27 @@ public class ReverseArray {
         System.out.println("Reversed array: " + Arrays.toString(numbers)); // [5, 4, 3, 2, 1]
     }
 }
+
+public class VowelConsonantCount {
+    public static void count(String input) {
+        int vowels = 0, consonants = 0;
+        input = input.toLowerCase();
+
+        for (char ch : input.toCharArray()) {
+            if (Character.isLetter(ch)) {
+                if ("aeiou".indexOf(ch) != -1) {
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+            }
+        }
+
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+    }
+
+    public static void main(String[] args) {
+        count("Java Programming"); // Vowels: 5, Consonants: 9
+    }
+}
