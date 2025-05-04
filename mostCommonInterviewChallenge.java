ed4be70ef0780b87c0bf64e6b304ccba8fb744f4
Jaspreet Singh <99jasingh@gmail.com>
2025-05-03 21:23:24 -0400

    starpattern

--- mostCommonInterviewChallenge.java
+++ mostCommonInterviewChallenge.java
@@ -999,3 +999,18 @@ public class EqualArrays {
         System.out.println("Arrays equal? " + areEqual(a, b)); // true
     }
 }
+
+public class StarPattern {
+    public static void printTriangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        printTriangle(5);
+    }
+}
