963174624dd73007877eae61768f02678590a236
Jaspreet Singh <99jasingh@gmail.com>
2025-05-31 19:07:03 -0400

    Power Recursive

--- mostCommonInterviewChallenge.java
+++ mostCommonInterviewChallenge.java
@@ -1384,3 +1384,14 @@ public class PrimeBetweenRange {
         printPrimes(10, 30); // 11 13 17 19 23 29
     }
 }
+
+public class PowerRecursive {
+    public static int power(int base, int exp) {
+        if (exp == 0) return 1;
+        return base * power(base, exp - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("2^5 = " + power(2, 5)); // 32
+    }
+}
