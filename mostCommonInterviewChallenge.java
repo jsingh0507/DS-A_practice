88416b1b317bd10438b7c94b0581cc6579230c3e
Jaspreet Singh <99jasingh@gmail.com>
2025-06-07 14:56:20 -0400

    Swap WithoutTemp

--- mostCommonInterviewChallenge.java
+++ mostCommonInterviewChallenge.java
@@ -1435,3 +1435,16 @@ public class AsciiValue {
         printAscii('z'); // 122
     }
 }
+
+public class SwapWithoutTemp {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        System.out.println("Before swap: a = " + a + ", b = " + b);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After swap: a = " + a + ", b = " + b);
+    }
+}
