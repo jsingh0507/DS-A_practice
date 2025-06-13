d620fbe61ea42f2f3dd35959931131d1db467e46
Jaspreet Singh <99jasingh@gmail.com>
2025-06-13 18:48:04 -0400

    Hollow Rectangle

--- mostCommonInterviewChallenge.java
+++ mostCommonInterviewChallenge.java
@@ -1571,3 +1571,21 @@ public class RemoveVowels {
         System.out.println(removeVowels("Java is Awesome")); // Jv s wsm
     }
 }
+
+public class HollowRectangle {
+    public static void printHollowRect(int rows, int cols) {
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                if (i == 1 || i == rows || j == 1 || j == cols)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        printHollowRect(5, 7);
+    }
+}
