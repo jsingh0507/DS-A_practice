a82569eb1b59fbdec6043053c24bec0d7add1ec6
Jaspreet Singh <99jasingh@gmail.com>
2025-06-13 18:46:39 -0400

    Average of array

--- mostCommonInterviewChallenge.java
+++ mostCommonInterviewChallenge.java
@@ -1528,3 +1528,16 @@ public class ArmstrongCheck {
         System.out.println("123 is Armstrong? " + isArmstrong(123)); // false
     }
 }
+public class AverageOfArray {
+    public static double average(int[] arr) {
+        int sum = 0;
+        for (int num : arr) sum += num;
+        return (double) sum / arr.length;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {10, 20, 30, 40, 50};
+        System.out.println("Average: " + average(nums)); // 30.0
+    }
+}
+
